580f77532beaa581f535093cdba026f20d8c22f6
aazamitsu <aazamitsu@outlook.jp>
2025-12-19 00:41:09 +0000

    あ

---
+++ scripts/main.js
@@ -0,0 +1,67 @@
+let textures = {};
+let lastType = undefined;
+let fade = 0;
+let squish = 0;
+let reloads = [];
+
+Events.run(ClientLoadEvent, e => {
+    Vars.content.units().each(u => {
+        let file = Vars.tree.get("chibis/" + u.name + ".png");
+        if(file.exists()){
+            textures[u] = new Texture(file);
+            textures[u].setFilter(Texture.TextureFilter.linear);
+        }
+    });
+
+    let config = {}
+    config[UnitTypes.alpha] = {
+        anchor: true
+    }
+    
+    let elem = extend(Element, {
+        draw(){
+            let width = Math.min(Scl.scl(300), Core.graphics.getWidth()/2);
+            if(!Vars.player.dead()){
+                let next = Vars.player.unit().type;
+                if(lastType != next){
+                    fade = Mathf.approachDelta(fade, 0, 0.04);
+                    if(fade <= 0.01){
+                        lastType = next;
+                        reloads = Array(Vars.player.unit().mounts.length);
+                        squish = 1;
+                    }
+                }else{
+                    fade = Mathf.approachDelta(fade, 1, 0.04);
+                }
+
+                if(reloads.length == Vars.player.unit().mounts.length){
+                    for(var i = 0; i < reloads.length; i ++){
+                        let val = Vars.player.unit().mounts[i].reload;
+                        if(reloads[i] < val){
+                            squish = 0.4;
+                        }
+                        reloads[i] = val;
+                    }
+                }
+	        }else{
+                fade = Mathf.approachDelta(fade, 0, 0.04);
+	        }
+	        
+	        if(lastType && textures[lastType] && Vars.state.isGame()){
+                let conf = config[lastType] || {}
+                let fin = Interp.swingOut.apply(fade);
+                let hOffset = 0;
+	            let tex = Draw.wrap(textures[lastType]);
+	            let height = width * tex.height / tex.width;
+                let squishFactor = 0.2 * squish + Mathf.sin(Time.time, 20, 0.01);
+                let floatScl = 50, floatMag = 8;
+                let ox = Mathf.sin(Time.time, 100, floatMag * 0.25), oy = Mathf.cos(Time.time + 5, floatScl, floatMag);
+	            Draw.rect(tex, width/2 + ox, -height * (1.0 - (conf.anchor ? Math.min(fin, 1) : fin)) + height/2 - height * hOffset + (conf.anchor ? 0 : oy) - 1, width * (1 + squishFactor), height * (1 - (conf.anchor ? 0 : squishFactor)));
+	        }
+
+            squish = Mathf.approachDelta(squish, 0, 0.05);
+        }
+    });
+    elem.touchable = Touchable.disabled;
+    Vars.ui.hudGroup.addChildAt(0, elem);
+})
